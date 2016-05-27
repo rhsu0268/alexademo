17c200693afbc0d17922c1847438d9c47f46e031
Richard Hsu <rhsu@gwu.edu>
2016-05-27 16:51:45 -0400

    Testing

--- app.rb
+++ app.rb
@@ -29,8 +29,11 @@ post '/' do
     @characterLower = @character.downcase
     puts @characterLower
     if @characterLower == 'the force awakens' 
-      
+
       @character.split.map(&:capitalize).join(' ')
+
+      puts "Capitalizing "
+      puts @character
       films = getFilms()
       result = getFilmCrawl(films, @character)
       puts "---FILMS---"
